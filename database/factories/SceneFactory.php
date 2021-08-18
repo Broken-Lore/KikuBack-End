f2d522921515e3fb50612bf5f123e64a61acdfdd
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-08-18 11:47:39 +0200

    Models created

---
+++ database/factories/SceneFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Scene;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SceneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Scene::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
